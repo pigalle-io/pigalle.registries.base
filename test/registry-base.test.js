c3e31e7583d6656d537ccb31279d7285c5618143
François Michaud <fmichaud@9fevrier.com>
2018-02-15 17:19:09 +0100

    Initial import

---
+++ test/registry-base.test.js
@@ -0,0 +1,10 @@
+/**
+ * This file is part of pigalle.registries.base
+ *
+ * Copyright (c) 2018 SAS 9 Février.
+ *
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ *
+ */
+
+const should = require('should');
