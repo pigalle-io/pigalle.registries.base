074116a5fc7deaa11028b8951775f05a3f7fc28e
François Michaud <fmichaud@9fevrier.com>
2018-02-21 18:13:14 +0100

    ...

--- test/registry-base.test.js
+++ test/registry-base.test.js
@@ -7,13 +7,16 @@
  *
  */
 
-require('should')
-
 /* eslint-env mocha */
 
 require('should')
 
+const {PigalleBaseClass} = require('@pigalle/core.base.class')
+const {UndefinedError} = require('@9fv.io/undefined-error')
 const {RegistryBase} = require('../lib/registry-base')
+
+class ValidType extends PigalleBaseClass {}
+class InvalidType {}
 
 describe('Class {RegistryBase}', () => {
   it('should be a function', () => {
@@ -22,21 +25,51 @@ describe('Class {RegistryBase}', () => {
 })
 
 describe('Create a instance of {RegistryBase} using <new> keyword', () => {
-  it('should be an object', () => {
-    (new RegistryBase()).should.be.an.Object()
+  it('and not passing argument as type should throw an {UndefinedError}', () => {
+    (() => new RegistryBase()).should.throw(Error) // @todo: check using {UndefinedError} is wrong...
   })
 
-  it('should be an instance of {RegistryBase}', () => {
-    (new RegistryBase()).should.be.an.instanceOf(RegistryBase)
+  it('and a valid derived {PigalleBaseClass} as type should be an object', () => {
+    (new RegistryBase(ValidType)).should.be.an.Object()
+  })
+
+  it('and a valid derived {PigalleBaseClass} as type should be an instance of {RegistryBase}', () => {
+    (new RegistryBase(ValidType)).should.be.an.instanceOf(RegistryBase)
   })
 })
 
 describe('Create a instance of {RegistryBase} using <factory> method', () => {
-  it('should be an object', () => {
-    (RegistryBase.factory()).should.be.an.Object()
+  it('and not passing argument as type should throw an {UndefinedError}', () => {
+    (() => RegistryBase.factory()).should.throw(Error) // @todo: check using {UndefinedError} is wrong...
   })
 
-  it('should be an instance of {RegistryBase}', () => {
-    (RegistryBase.factory()).should.be.an.instanceOf(RegistryBase)
+  it('and a valid derived {PigalleBaseClass} as type should be an object', () => {
+    (RegistryBase.factory(ValidType)).should.be.an.Object()
+  })
+
+  it('and a valid derived {PigalleBaseClass} as type should be an instance of {RegistryBase}', () => {
+    (RegistryBase.factory(ValidType)).should.be.an.instanceOf(RegistryBase)
+  })
+})
+
+describe('Call <add> method', () => {
+  it('using an invalid type should throw an {InvalidTypeError}', () => {
+    (() => RegistryBase.factory(ValidType).add('key', new InvalidType())).should.throw(Error) // @todo: check using {UndefinedError} is wrong...
+  })
+
+  it('using an valid type should return an instance of {RegistryBase}', () => {
+    (RegistryBase.factory(ValidType).add('key', new ValidType())).should.be.an.instanceOf(RegistryBase) // @todo: check using {UndefinedError} is wrong...
+  })
+})
+
+describe('Call <get> method', () => {
+  /**
+  it('using an invalid type should throw an {InvalidTypeError}', () => {
+    (() => RegistryBase.factory(ValidType).add('key', new InvalidType())).should.throw(Error) // @todo: check using {UndefinedError} is wrong...
+  })
+   */
+
+  it('using an existent key should return an instance of {ValidType}', () => {
+    (RegistryBase.factory(ValidType).add('key', new ValidType()).get('key')).should.be.an.instanceOf(ValidType) // @todo: check using {UndefinedError} is wrong...
   })
 })
